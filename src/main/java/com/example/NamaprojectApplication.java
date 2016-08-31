449f8f6364f29a3e76c7c2488ebccf6f232f9059
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2016-08-31 16:37:07 +0700

    First commit

---
+++ src/main/java/com/example/NamaprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamaprojectApplication.class, args);
+	}
+}
